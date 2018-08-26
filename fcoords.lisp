2ee210f3c0171da692a33aef6137770437109364
Alejandro Zamora <ale2014.zamora@gmail.com>
2018-08-26 04:24:57 -0300

    - Minor change

--- fcoords.lisp
+++ fcoords.lisp
@@ -2,8 +2,6 @@
 
 (in-package #:fcoords)
 
-;;; "fcoords" goes here. Hacks and glory await!
-
 (defun back-transformation (x y f &optional (g #'identity))
   "Converts a single point transformed to normal coordinates"
   (values (- (funcall g x) (funcall f y))
